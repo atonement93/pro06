0c064a0a3ef7598c5f091fac87fec8137c9dd0f4
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-11-03 22:27:27 +0900

    서블릿 개발 환경 설정

--- src/main/java/sec01/ex01/InputServlet.java
+++ src/main/java/sec01/ex01/InputServlet.java
@@ -1,7 +1,6 @@
 package sec01.ex01;
 
 import java.io.IOException;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,31 +8,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class LoginServlet
+ * Servlet implementation class InputServlet
  */
-@WebServlet("/login")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/input")
+public class InputServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
     /**
      * Default constructor. 
      */
-    public LoginServlet() {
-        System.out.println();
+    public InputServlet() {
+        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see Servlet#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
