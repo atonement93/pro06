6b6d2ca2a7da3a84fdd6308722f0dd9ffbe09066
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-11-04 19:56:38 +0900

    서블릿으로 클라이언트 요청 받기 연습

--- src/main/java/sec01/ex01/LoginServlet.java
+++ src/main/java/sec01/ex01/LoginServlet.java
@@ -1,6 +1,7 @@
 package sec01.ex01;
 
 import java.io.IOException;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,25 +9,43 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class InputServlet
+ * Servlet implementation class LoginServlet
  */
-@WebServlet("/input")
-public class InputServlet extends HttpServlet {
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
     /**
-     * Default constructor. 
+     * @see HttpServlet#HttpServlet()
      */
-    public InputServlet() {
+    public LoginServlet() {
+        super();
         // TODO Auto-generated constructor stub
     }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("utf-8");
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		System.out.println("아이디 : "+user_id);
+		System.out.println("비밀번호 : "+user_pw);
 	}
 
 	/**
