1b36f4be4ea8944186356bcbaf4208b797bcea3b
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-11-08 17:50:00 +0900

    회원가입

---
+++ src/main/java/sec02/ex02/MemberDAO.java
@@ -0,0 +1,119 @@
+package sec02.ex02;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import sec02.ex01.MemberVO;
+
+public class MemberDAO {
+	
+public List<sec02.ex02.MemberVO> listMembers() {
+		
+		List<sec02.ex02.MemberVO> list = new ArrayList<sec02.ex02.MemberVO>();
+		
+//		String user = "user02";
+//		String password = "user02";
+//		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+//		String driver = "oracle.jdbc.driver.OracleDriver";
+		
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			DataSource dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+			
+			//Class.forName(driver);
+			
+			//Connection con = DriverManager.getConnection(url, user, password);
+			
+			Connection con = dataFactory.getConnection();
+			
+			String query = "SELECT * FROM T_MEMBER";
+			
+			//PrepareStatement ¿¬½À
+			PreparedStatement pstmt = con.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery();
+			
+			//Statement stmt = con.createStatement();
+			
+			//ResultSet rs = stmt.executeQuery(query);
+			
+			while(rs.next()) {
+				String id = rs.getString("id");
+				String pw = rs.getString("pw");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				
+				sec02.ex02.MemberVO vo = new sec02.ex02.MemberVO();
+				vo.setId(id);
+				vo.setPw(pw);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setJoinDate(joinDate);
+				
+				list.add(vo);
+
+			}
+			
+			rs.close();
+			pstmt.close();
+			con.close();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+		
+	}
+	
+	public void addMember(sec02.ex02.MemberVO memberVO) {
+
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			DataSource dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+
+			Connection con = dataFactory.getConnection();
+			
+			String id = memberVO.getId();
+			String pw = memberVO.getPw();
+			String name = memberVO.getName();
+			String email = memberVO.getEmail();
+			Date joinDate = memberVO.getJoinDate();
+			
+			String query = "INSERT INTO T_MEMBER";
+			query +=" (id,pw,name,email)";
+			query +=" values(?,?,?,?)";
+			System.out.println("prepareStatement : "+query);
+			PreparedStatement pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pw);
+			pstmt.setString(3, name);
+			pstmt.setString(4, email);
+			ResultSet rs = pstmt.executeQuery();
+
+			
+			rs.close();
+			pstmt.close();
+			con.close();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
+
+
---
+++ src/main/java/sec02/ex02/MemberServlet.java
@@ -0,0 +1,114 @@
+package sec02.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberServlet
+ */
+@WebServlet("/member3")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> list = dao.listMembers();
+		
+		out.print("<html><body>");
+		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
+		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
+		
+		for(int i=0;i<list.size();i++) {
+			MemberVO memberVO = list.get(i);
+			String id=memberVO.getId();
+			String pw=memberVO.getPw();
+			String name=memberVO.getName();
+			String email=memberVO.getEmail();
+			Date joinDate=memberVO.getJoinDate();
+			out.print("<tr><td>"+id+"</td><td>"+pw+"</td><td>"+name+"</td><td>"+email+"</td><td>"+joinDate+"</td></tr>");
+		}
+		out.print("</table></body><html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		MemberDAO dao = new MemberDAO();
+		PrintWriter out = response.getWriter();
+		
+		String command = request.getParameter("command");
+		
+		if (command != null && command.equals("addMember")) {
+			String id = request.getParameter("id");
+			String pw = request.getParameter("pw");
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			MemberVO vo = new MemberVO();
+			vo.setId(id);
+			vo.setPw(pw);
+			vo.setName(name);
+			vo.setEmail(email);
+			dao.addMember(vo);
+		}
+//			else if(command != null && command.equals("delMember")){
+//			String id = request.getParameter(id);
+//			dao.delMember(id);
+//		}
+		List<MemberVO> list = dao.listMembers();
+		out.print("<html><body>");
+		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
+		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td><td>삭제</td></tr>");
+		
+		for(int i=0;i<list.size();i++) {
+			MemberVO memberVO = (MemberVO) list.get(i);
+			String id = memberVO.getId();
+			String pw = memberVO.getPw();
+			String name = memberVO.getName();
+			String email = memberVO.getEmail();
+			Date joinDate = memberVO.getJoinDate();
+			out.print("<tr><td>" + id + "</td><td>" + pw + "</td><td>" + name + "</td><td>" + email + "</td><td>" + joinDate + "</td><td>" + "<a href='/pro06/member3?command=delMember&id=" + id + "'>삭제</a></td></tr>");
+		}
+		out.print("</table></body></html>");
+		out.print("<a href='/pro06/memberForm.html'>새 회원가입하기</a>");
+	}
+
+}
